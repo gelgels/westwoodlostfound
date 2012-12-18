261095a3e02be7cb4463dce56473a6cf8974f161
Angela Navarro <angelarnavarro@gmail.com>
2012-12-17 16:21:04 -0800

    Skeleton for the app
    - class for the DB model representing a "posting"
    - front page executes a query to the DB for recent postings
    - submit form takes user input and puts into DB

--- main.py
+++ main.py
@@ -14,12 +14,81 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import cgi
+import urllib
 import webapp2
+import datetime
+
+from google.appengine.ext import db
+
+
+class Posting(db.Model):
+    """ Models a posting on the site """
+    title = db.StringProperty()
+    desc = db.StringProperty(multiline=True)
+    email = db.StringProperty()
+    category = db.StringProperty()
+    date = db.DateTimeProperty(auto_now_add=True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+
+        self.response.write("""
+            <html>
+                <body>
+                    <div class="header">
+                        Westwood Lost and Found
+                    </div>
+                    <div class="submit-post">
+                        <a href="/submit">Submit a post</a>
+                    </div>
+        """)
+
+        postings = db.GqlQuery("SELECT * "
+                               "FROM Posting "
+                               "ORDER BY date DESC LIMIT 10")
+        for p in postings:
+            self.response.out.write('<div class="post">')
+            self.response.out.write('<div class="post-title">%s</div>' % p.title)
+            self.response.out.write('<div class="post-desc">%s</div>' % p.desc)
+            self.response.out.write('<div class="post-date">%s</div>' % p.date)
+            self.response.out.write('<div class="post-category">%s</div>' % p.category)
+            self.response.out.write('</div>')
+
+        self.response.write("""
+                </body>
+            </html>
+        """)
+
+class SubmitPostView(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Submit a lost or found item below:')
+        self.response.write("""
+            <html>
+                <body>
+                    <form action="/submit-post" method="post">
+                        <div> <textarea name="title"></textarea> </div>
+                        <div> <textarea name="desc"></textarea> </div>
+                        <div> <textarea name="email"></textarea> </div>
+                        <div> <input type="submit"> </div>
+                    </form>
+                </body>
+            </html>
+            """)
+
+class SubmitPost(webapp2.RequestHandler):
+    def post(self):
+        p = Posting()
+        p.title = self.request.get('title')
+        p.desc = self.request.get('desc')
+        p.email = self.request.get('email')
+        p.category = 'lost'
+        p.put()
+
+        self.response.out.write("Your posting about %s has been posted." % self.request.get('title'))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/submit', SubmitPostView),
+    ('/submit-post', SubmitPost)
 ], debug=True)
