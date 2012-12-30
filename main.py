4ce3d03dea453bc215de9075fd76aca310d8b1f2
Angela Navarro <angelarnavarro@gmail.com>
2012-12-30 14:05:56 -0800

    Added contact functionality and unsubscribing

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ import datetime
 import wsgiref.handlers
 
 from google.appengine.ext import db
+from google.appengine.api import mail
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -36,6 +37,7 @@ class Posting(db.Model):
     email = db.StringProperty()
     category = db.StringProperty()
     date = db.DateTimeProperty(auto_now_add=True)
+    allow_contact = db.BooleanProperty()
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -73,6 +75,7 @@ class SubmitPost(webapp2.RequestHandler):
             p.desc = self.request.get('desc')
             p.email = self.request.get('email')
             p.category = self.request.get('category')
+            p.allow_contact = True
             p.put()
 
             template_values = {
@@ -114,11 +117,80 @@ class PostView(webapp2.RequestHandler):
         template = jinja_environment.get_template('post.html')
         self.response.out.write(template.render(template_values))
 
+class ContactView(webapp2.RequestHandler):
+    def get(self):
+        id_arg = self.request.get('id')
+        post = Posting.get_by_id(int(id_arg))
+
+        template_values = {
+            'allow_contact' : post.allow_contact,
+            'id'            : id_arg,
+            'title'         : post.title
+        }
+
+        template = jinja_environment.get_template('contact.html')
+        self.response.out.write(template.render(template_values))
+
+class ContactSubmit(webapp2.RequestHandler):
+    def post(self):
+        post_id = self.request.get("id")
+        sender_email = self.request.get("email")
+        sender_message = self.request.get("message")
+
+        post = Posting.get_by_id(int(post_id))
+        allow_contact = post.allow_contact
+        recipient_email = post.email
+
+        if self.request.get("spam-check"):
+            logging.info("Attempted spam email via contact form")
+        elif allow_contact:
+            body = """
+            You have received a message regarding your post titled %s on Westwood Lost and Found.
+
+            Message:
+            %s
+
+            If you wish to respond, contact the following:
+            %s
+
+            If you don't want to receive emails regarding your post, click here:
+            http://localhost:8081/unsub?key=%s
+            """ % (post.title, sender_message, sender_email, post.key())
+
+            mail.send_mail(sender="Westwood Lost and Found <mail@westwoodlostfound.appspotmail.com>",
+                           to=recipient_email,
+                           subject="Response to '" + post.title + "'",
+                           body=body)
+
+        template_values = {
+            'id'            : post_id,
+            'title'         : post.title,
+            'allow_contact' : allow_contact
+        }
+
+        template = jinja_environment.get_template('contact-confirm.html')
+        self.response.out.write(template.render(template_values))
+
+class Unsubscribe(webapp2.RequestHandler):
+    def get(self):
+        post_key = self.request.get('key')
+        post = Posting.get(post_key)
+
+        post.allow_contact = False
+        post.put()
+
+        template = jinja_environment.get_template('unsub.html')
+        self.response.out.write(template.render())
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/post', PostView),
     ('/lost', LostItemsView),
     ('/found', FoundItemsView),
     ('/submit', SubmitPostView),
-    ('/submit-post', SubmitPost)
+    ('/submit-post', SubmitPost),
+    ('/contact', ContactView),
+    ('/contact-submit', ContactSubmit),
+    ('/unsub', Unsubscribe)
 ], debug=True)
